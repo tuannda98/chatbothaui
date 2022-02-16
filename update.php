d0b1c5299ef386e859a72ed2d0918c95dde6b6bb
tuannda98 <63991938+tuannda98@users.noreply.github.com>
2022-02-16 16:04:31 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'config.php'; //lấy thông tin từ config
 $conn = mysqli_connect($DBHOST, $DBUSER, $DBPW, $DBNAME) or die ('Không thể kết nối tới database');
-$ID = $_POST['sender_id'];// lấy id từ chatfuel
-// $gioitinh = $_POST['gt']; // lấy giới tính
-$gioitinh = 0;
+$ID = $_POST['ID'];// lấy id từ chatfuel
+$gioitinh = $_POST['gt']; // lấy giới tính
+// $gioitinh = 0;
 function isUserExist($userid) { //hàm kiểm tra xem user đã tồn tại chưa 
   global $conn;
   $result = mysqli_query($conn, "SELECT `ID` from `users` WHERE `ID` = $userid LIMIT 1");
@@ -12,11 +12,11 @@ function isUserExist($userid) { //hàm kiểm tra xem user đã tồn tại chư
 }
 
 /// Xét giới tính
-// if ($gioitinh == 'male'){
-// $gioitinh = 1;
-// } else if ($gioitinh == 'female'){
-// $gioitinh = 2;
-// }
+if ($gioitinh == 'male'){
+$gioitinh = 1;
+} else if ($gioitinh == 'female'){
+$gioitinh = 2;
+}
 
 if ( !isUserExist($ID) ) { // nếu chưa tồn tại thì update lên sever
     $sql = "INSERT INTO `users` (`ID`, `trangthai`, `hangcho` ,`gioitinh`) VALUES (".$ID.", 0, 0 , $gioitinh)";
